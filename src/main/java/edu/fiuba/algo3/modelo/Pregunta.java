adeb1185dc7c8eb8de5e46ceef057a72ae793585
licha <lisandrotorresetti@hotmail.com>
2020-07-24 15:52:08 -0300

    Agrego unos cambios, no va a pasar los tests, pusheo para que se suba el nuevo branch

--- src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Pregunta.java
@@ -4,12 +4,35 @@ import java.util.ArrayList;
 public abstract class Pregunta {
 
     public String enunciado;
-    public ArrayList<Opcion> opciones;
-    protected ArrayList<Respuesta> respuestas; // Por simplicidad lo coloque momentaneamete unico y protegido
-    protected EstadoPuntaje estadoPuntaje;
-    protected ArrayList<Respuesta> respuestasCorrectas;
+    public ArrayList<String> opciones; //cuando creamos la pregunta se le pasa una lista de opciones
+    //protected ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>(); // Por simplicidad lo coloque momentaneamete unico y protegido
+    protected int puntos;
+    protected Respuesta respuestaCorrecta;
 
 
-    public abstract int evaluarRespuesta (Respuesta respuestaJugador);
+    public void imprimirEnunciado(){
+        System.out.println(enunciado);
+    }
+
+    public void imprimirOpciones(){
+        for (String opcion: opciones){
+            System.out.println(opcion);
+        }
+    }
+
+    public int getPuntos(){
+        return puntos;
+    }
+
+
+    //METODOS ABSTRACTOS
+    public abstract void evaluarRespuesta(ArrayList<Respuesta> respuestasJugadores); //Cada pregunta deberia saber como evaluar las respuestas
+
+    public abstract void imprimirRespuestaCorrecta();
+
+
+
+
+
 
 }
---
+++ src/main/java/edu/fiuba/algo3/modelo/VerdaderoOFalsoClasico.java
@@ -0,0 +1,30 @@
+package edu.fiuba.algo3.modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class VerdaderoOFalsoClasico extends Pregunta {
+
+    public VerdaderoOFalsoClasico(String enunciado, Respuesta respuestaCorrecta, ArrayList<String> opciones){
+        this.enunciado = enunciado;
+        this.opciones = opciones;
+        this.respuestaCorrecta = respuestaCorrecta;
+        super.puntos = 1;
+    }
+
+    @Override
+    public void evaluarRespuesta(ArrayList<Respuesta> respuestasJugadores) {
+        for(Respuesta respuestaJugador: respuestasJugadores){
+            if (this.respuestaCorrecta.compararCon(respuestaJugador)){
+                respuestaJugador.asignarPuntaje(this.puntos);
+            }
+
+        }
+
+    }
+
+    @Override
+    public void imprimirRespuestaCorrecta() {
+        String respuestaCorrecta = (this.respuestaCorrecta.getRespuesta()).get(0);
+    }
+}
