ff5f4a88501948f743516bd6ac40f38d03e20a8f
alejorodriguez96 <atrodriguez@fi.uba.ar>
2020-07-27 23:13:01 -0300

    refactor Borrado metodos no ultilizado

--- src/main/java/edu/fiuba/algo3/modelo/correccion/Resultado.java
+++ src/main/java/edu/fiuba/algo3/modelo/correccion/Resultado.java
@@ -6,22 +6,11 @@ public class Resultado {
     int incorrectas;
     int correctasEsperadas;
 
-    public Resultado () {
-        this(0,0,0);
-    }
 
     public Resultado(int correctas, int incorrectas, int correctasEsperadas) {
         this.correctas = correctas;
         this.incorrectas = incorrectas;
         this.correctasEsperadas = correctasEsperadas;
-    }
-
-    public void setCorrectas(int correctas) {
-        this.correctas = correctas;
-    }
-
-    public void setIncorrectas(int incorrectas) {
-        this.incorrectas = incorrectas;
     }
 
     public int getCorrectas() {
