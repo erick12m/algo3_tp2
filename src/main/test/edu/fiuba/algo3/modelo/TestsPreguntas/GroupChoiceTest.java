037f94d3f937ec1a1c07926a1ef9574badda4bfe
Erick Martinez <erickm1299@gmail.com>
2020-08-01 19:36:06 -0300

    Agrego test

--- src/main/test/edu/fiuba/algo3/modelo/TestsPreguntas/GroupChoiceTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/TestsPreguntas/GroupChoiceTest.java
@@ -125,4 +125,69 @@ public class GroupChoiceTest {
         assertEquals(1, jugador1.getPuntaje());
         assertEquals(0, jugador2.getPuntaje());
     }
+    @Test
+    public void test05AmbosJugadoresAgrupanIncorrectamenteYNingunoSumaPuntos() {
+        CorrectorClasico clasico = new CorrectorClasico();
+        GroupChoice pregunta = new GroupChoice("Separe pares de impares", respuestaCorrecta, opciones, clasico);
+
+        var grupo1Jugador1 = new ArrayList<String>();
+        var grupo2Jugador1 = new ArrayList<String>();
+        var grupo1Jugador2 = new ArrayList<String>();
+        var grupo2Jugador2 = new ArrayList<String>();
+        //Las respuestas de cada uno están desordenadas para este test
+        grupo1Jugador1.add("dos");
+        grupo1Jugador1.add("uno");
+        grupo2Jugador1.add("cuatro");
+        grupo2Jugador1.add("tres");
+
+        grupo1Jugador2.add("uno");
+        grupo1Jugador2.add("cuatro");
+        grupo2Jugador2.add("dos");
+        grupo2Jugador2.add("tres");
+        var respuestaJugador1 = new Respuesta(grupo1Jugador1,grupo2Jugador1);
+        var respuestaJugador2 = new Respuesta(grupo1Jugador2,grupo2Jugador2);
+        jugador1.respuestaElegida(respuestaJugador1);
+        jugador2.respuestaElegida(respuestaJugador2);
+
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(jugador1.getRespuesta());
+        respuestas.add(jugador2.getRespuesta());
+        pregunta.evaluarRespuesta(respuestas);
+        jugador1.actualizarPuntaje();
+        jugador2.actualizarPuntaje();
+        assertEquals(0, jugador1.getPuntaje());
+        assertEquals(0, jugador2.getPuntaje());
+    }
+
+    @Test
+    public void test06AmbosJugadoresAgrupanIncompletamenteYNingunoSumaPuntos() {
+        CorrectorClasico clasico = new CorrectorClasico();
+        GroupChoice pregunta = new GroupChoice("Separe pares de impares", respuestaCorrecta, opciones, clasico);
+
+        var grupo1Jugador1 = new ArrayList<String>();
+        var grupo2Jugador1 = new ArrayList<String>();
+        var grupo1Jugador2 = new ArrayList<String>();
+        var grupo2Jugador2 = new ArrayList<String>();
+        //Las respuestas de cada uno están desordenadas para este test
+        grupo1Jugador1.add("dos");
+        grupo2Jugador1.add("cuatro");
+
+        grupo1Jugador2.add("uno");
+        grupo2Jugador2.add("tres");
+        var respuestaJugador1 = new Respuesta(grupo1Jugador1,grupo2Jugador1);
+        var respuestaJugador2 = new Respuesta(grupo1Jugador2,grupo2Jugador2);
+        jugador1.respuestaElegida(respuestaJugador1);
+        jugador2.respuestaElegida(respuestaJugador2);
+
+
+        ArrayList<Respuesta> respuestas = new ArrayList<Respuesta>();
+        respuestas.add(jugador1.getRespuesta());
+        respuestas.add(jugador2.getRespuesta());
+        pregunta.evaluarRespuesta(respuestas);
+        jugador1.actualizarPuntaje();
+        jugador2.actualizarPuntaje();
+        assertEquals(0, jugador1.getPuntaje());
+        assertEquals(0, jugador2.getPuntaje());
+    }
 }
