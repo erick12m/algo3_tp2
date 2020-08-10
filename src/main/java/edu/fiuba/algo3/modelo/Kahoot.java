76b444f8c2c3c0f1554a1c3ea65936f9e0dbf492
Erick Martinez <erickm1299@gmail.com>
2020-08-10 11:16:41 -0300

    Resueltos comentarios del PR

--- src/main/java/edu/fiuba/algo3/modelo/Kahoot.java
+++ src/main/java/edu/fiuba/algo3/modelo/Kahoot.java
@@ -15,36 +15,6 @@ public class Kahoot {
     public static Jugador jugadorActual;
     private ArrayList<Pregunta> listaDePreguntas;
 
-    /*private Kahoot(String nombreJugador1, String nombreJugador2){
-        this.crearJugadores(nombreJugador1, nombreJugador2);
-        //listaDePreguntas = FabricaDePreguntas.crearPreguntas(); TODO implementar despues la fabrica de preguntas
-    }*/
-
-    public String getGanador(){
-        Jugador jugador1 = jugadores.get(0);
-        Jugador jugador2 = jugadores.get(1);
-        int puntosJugador1 = jugador1.getPuntaje();
-        int puntosJugador2 = jugador2.getPuntaje();
-
-        if (puntosJugador1 == puntosJugador2) return "Empate";
-
-        else if (puntosJugador1 > puntosJugador2) return jugador1.getNombre();
-
-        return jugador2.getNombre();
-    }
-
-
-    private void crearJugadores(String nombreJugador1, String nombreJugador2){
-        Jugador jugador1  = new Jugador(nombreJugador1);
-        Jugador jugador2 = new Jugador(nombreJugador2);
-
-        jugadores.add(jugador1);
-        jugadores.add(jugador2);
-
-        //El que comienza respondiendo se elige de manera random
-        double numRandom = Math.random();
-        jugadorActual = (numRandom > 0.5) ? jugador1 : jugador2;
-    }
 
     public void setPreguntaActual(Pregunta preguntaActual){
         Kahoot.preguntaActual = preguntaActual;
@@ -71,7 +41,7 @@ public class Kahoot {
         } catch (NoTieneExclusividadException e) {
         }
     }
-    // TODO ACORDARSE DE DEFINIR QUE HACER CON LA PREGUNTA ACTUAL Y EL PARAMETRO PREGUNTA
+
     public void puntuarPregunta(){
         var respuestas = this.obtenerRespuestas();
         preguntaActual.evaluarRespuesta(respuestas);
